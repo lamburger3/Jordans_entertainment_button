0fc62c170ab0d4fcff7ddc9c41b3d30d90c5facf
lamburger3 <lamburger3@gmail.com>
2017-11-28 23:02:05 -0800

    Slightly "Better" funbutton
    Opens Github now too, because why not?

--- funbutton.py
+++ funbutton.py
@@ -1,7 +1,7 @@
 import webbrowser
 import time
 
-funurls = ["www.youtube.com", "www.reddit.com"]
+funurls = ["www.youtube.com", "www.reddit.com", "www.github.com"]
 
 def open_tabs(url_list):
     for url in url_list:
